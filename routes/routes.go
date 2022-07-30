84a793b7a20a6212e9b585592703070e764dd4d7
inciner8r <punarvpawade17@gmail.com>
2022-07-30 17:20:13 +0530

    ok

--- routes/routes.go
+++ routes/routes.go
@@ -3,5 +3,5 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func routes(router *gin.Engine) {
-	router.POST("/newBlog", controllers.CreateBlog)
+	router.POST("/newBlog", controllers.CreateBlogs)
 }
