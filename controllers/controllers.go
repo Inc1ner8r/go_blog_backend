8529deee92794dd4904305c9482b1e205863fea0
inciner8r <punarvpawade17@gmail.com>
2022-08-02 16:53:34 +0530

    gorm

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,13 +5,25 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/inciner8r/blog_backend_go/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+func initDB() *gorm.DB {
+	dsn := "root:root@tcp(127.0.0.1:3306)/test"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+	}
+	return db
+}
+
+var db = initDB()
+
 func CreateBlog(c *gin.Context) {
-	//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	//defer cancel()
 
 	var blog models.Blog
+	results, err := db.Query("select * from test_table")
 
 	if err := c.BindJSON(&blog); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"database/sql"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/inciner8r/blog_backend_go/routes"
 )
 
 func main() {
-
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
 
 	r := gin.Default()
 	r.Use(cors.Default())
